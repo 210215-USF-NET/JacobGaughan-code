c25cd0e3b615b62db80045de1abba24dc3bf3948
Jacob Gaughan <jakegaughan5@gmail.com>
2021-02-16 15:27:17 -0600

    added Hero Demo

--- 1-csharp/TourOfHeroes/ToHModels/Hero.cs
+++ 1-csharp/TourOfHeroes/ToHModels/Hero.cs
@@ -24,6 +24,19 @@ namespace ToHModels
     /// </summary>
     public class Hero
     {
-        public string HeroName { get; set; }
+        private string heroName;
+        public string HeroName {
+            get { return heroName; }
+            set { 
+                if (value.Equals(null)){ } // TODO: throw exception
+                heroName = value;
+            }
+        }
+
+        public int HP { get; set; }
+
+        public Element ElementType { get; set; }
+
+        public SuperPower SuperPower { get; set; }
     }
 }
--- 1-csharp/TourOfHeroes/ToHUI/Program.cs
+++ 1-csharp/TourOfHeroes/ToHUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ToHModels;
 
 namespace ToHUI
 {
@@ -6,7 +7,24 @@ namespace ToHUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create hero method/logic
+            Hero newHero = new Hero();
+            Console.WriteLine("Enter Hero Name: ");
+            newHero.HeroName = Console.ReadLine();
+            Console.WriteLine("Enter HP value: ");
+            newHero.HP = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter SuperPower details");
+            SuperPower newSuperPower = new SuperPower();
+            Console.WriteLine("Enter SuperPower name: ");
+            newSuperPower.Name = Console.ReadLine();
+            Console.WriteLine("Enter SuperPower description: ");
+            newSuperPower.Description = Console.ReadLine();
+            Console.WriteLine("Enter SuperPower damage: ");
+            newSuperPower.Damage = int.Parse(Console.ReadLine());
+            newHero.SuperPower = newSuperPower;
+            Console.WriteLine("Set the element type of the hero: ");
+            newHero.ElementType = Enum.Parse<Element>(Console.ReadLine());
+            Console.WriteLine($"Hero Created with details: \n\t Name: {newHero.HeroName} \n\t SuperPower: {newHero.SuperPower.Name} \n\t Type: {newHero.ElementType}");
         }
     }
 }
