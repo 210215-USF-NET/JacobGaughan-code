85688f9b2c623d8e0b02e8257d9cb039c1097a61
Jacob Gaughan <jakegaughan5@gmail.com>
2021-02-17 11:58:27 -0600

    Exception handling

--- 1-csharp/TourOfHeroes/ToHUI/Program.cs
+++ 1-csharp/TourOfHeroes/ToHUI/Program.cs
@@ -9,7 +9,7 @@ namespace ToHUI
     {
         static void Main(string[] args)
         {
-            IMenu menu = new HeroMenu(new HeroBL(new HeroRepoSC()));
+            IMenu menu = new HeroMenu(new HeroBL(new HeroRepoFile()));
             menu.Start();
         }
     }
