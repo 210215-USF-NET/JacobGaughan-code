e10939c5e9e8ed3e185cf1845585896e63e59bed
Jacob Gaughan <jakegaughan5@gmail.com>
2021-02-18 16:31:40 -0600

    Serilog Logger Level Switching

--- 1-csharp/SerilogDemo/Program.cs
+++ 1-csharp/SerilogDemo/Program.cs
@@ -7,12 +7,33 @@ namespace SerilogDemo
     {
         static void Main()
         {
-            var logger = new LoggerConfiguration()
-                .WriteTo.File("logs/Logs.txt", rollingInterval: RollingInterval.Day)
+            
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.File("logs/Logs.json")
                 .CreateLogger();
-                
 
-            logger.Information("Hello, this is Serilog!");
+            Log.Verbose("Verbose log message");
+            Log.Debug("Debug log message");
+            Log.Information("Information log message");
+            Log.Warning("Warning log message");
+            Log.Error("Error log message");
+            Log.Fatal("Fatal log message");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
