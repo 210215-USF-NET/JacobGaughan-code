fe73d4f81b15ba5b79baed8d67d1149185a87c8e
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-19 12:49:42 -0500

    js intro

--- 1-csharp/HelloWorld/Program.cs
+++ 1-csharp/HelloWorld/Program.cs
@@ -9,6 +9,18 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello {name}!");
+
+            int[][] obstacles = new int[][]
+            {
+                new int[] { 1, 3 },
+                new int[] { 0, 2},
+                new int[] { 11, 22}
+            };
+
+            foreach(int[] item in obstacles)
+        {
+            Console.WriteLine($"obstacleY {item[0]} - obstacleX {item[1]}");
+        }
         }
     }
 }
